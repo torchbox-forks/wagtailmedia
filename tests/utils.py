960b59a9384f4edadb64f52786dabfd4d4e512d8
zerolab <dan@zerolab.org>
2025-10-18 11:53:46 +0100

    Furthere GH release action tweaks

--- tests/utils.py
+++ tests/utils.py
@@ -1,13 +1,32 @@
 import re
+import shutil
+import tempfile
 
 from typing import Optional
 
 from django.core.files.base import ContentFile
+from django.test import TestCase
 
 from wagtailmedia.models import MediaType, get_media_model
 
 
 Media = get_media_model()
+
+
+class TempDirMediaRootMixin(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.tmpdir = tempfile.mkdtemp()
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(cls.tmpdir, ignore_errors=True)
+
+    def run(self, result=None):
+        with self.settings(MEDIA_ROOT=self.tmpdir):
+            return super().run(result)
 
 
 def create_media(
